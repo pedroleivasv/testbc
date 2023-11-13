26511b152c397e6b163c0c833ac5233d14376447
pedroleivasv <pedroleiva.sv@gmail.com>
2023-11-13 17:39:02 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Museum of candy
+
+This is a project to demonstrate skills in HTML, CSS and Bootstrap. It has a small script using JavaScript with JQuery but the main focus is the Bootstrap part.
+
+[--Live Demo--](https://raw.githack.com/jDavid95/Museum-of-Candy/master/index.html "Live Demo")
---
+++ main.js
@@ -0,0 +1,16 @@
+$(function () {
+    "use strict";
+    $.scrollIt({
+	easing: 'swing',
+	scrollTime: 900,
+	activeClass: 'active',
+	onPageChange: null,
+	topOffset: -70,
+	upKey: 38,
+        downKey: 40
+	  });
+    $(document).scroll(function () {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });	    
+});
